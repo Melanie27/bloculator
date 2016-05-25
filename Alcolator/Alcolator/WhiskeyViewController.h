acd8d2f24680af5856428b5d01147c22476e1991
Melanie27 <melaniemcganney@gmail.com>
2016-05-24 18:02:30 -0700

    checkpoint 20 begun

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by MELANIE MCGANNEY on 5/24/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
